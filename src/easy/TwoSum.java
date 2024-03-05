f239d72e3c767159207fa5755230da4788d6dcc2
whiteclouds <whiteclouds1971@163.com>
2024-03-05 09:15:37 +0800

    feature:towsum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,11 +1,50 @@
 package easy;
 
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
 public class TwoSum {
+    /**
+     *  55ms;43.78MB
+     */
     int[] twoSum(int[] nums, int target) {
-        return nums;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return null;
     }
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{3, 3}, 6)));
     }
 }
